cfd04a4e8691f2ac78957da88ca4d51dfa289599
Amir <amirfadhil.abd@gmail.com>
2024-08-29 07:48:03 +0300

    Create DatabaseInitilization.kt

---
+++ app/src/main/java/com/example/marvel_app/util/DatabaseInitilization.kt
@@ -0,0 +1,52 @@
+// DependencyInitializer.kt
+package com.example.marvel_app.util
+
+import android.content.Context
+import androidx.room.Room
+import com.example.marvel_app.data.data_source.local.MarvelDatabase
+import com.example.marvel_app.data.network.RetrofitClient
+import com.example.marvel_app.data.repository.MarvelRepositoryImpl
+import com.example.marvel_app.domain.usecase.FetchCharacterDetailsUseCase
+import com.example.marvel_app.domain.usecase.FetchCharactersUseCase
+import com.example.marvel_app.presentation.viewmodel.Marvels_Screen
+import com.example.marvel_app.presentation.viewmodel.CharacterViewModel
+
+
+fun initializeDependencies(context: Context): Pair<CharacterViewModel, Marvels_Screen> {
+    // Initialize the database
+    val db = Room.databaseBuilder(
+        context.applicationContext, MarvelDatabase::class.java, "MarvelDatabase"
+    ).build()
+
+    // Initialize DAOs
+    val characterDao = db.characterDao()
+    val comicDao = db.comicDao()
+    val seriesDao = db.seriesDao()
+    val eventDao = db.eventDao()
+
+
+
+    // Initialize the API service
+    val apiService = RetrofitClient.apiService
+
+    // Initialize the repository with all the dependencies
+    val repository = MarvelRepositoryImpl(
+        apiService = apiService,
+        characterDao = characterDao,
+        comicDao = comicDao,
+        seriesDao = seriesDao,
+        eventDao = eventDao,
+
+    )
+
+    // Initialize use cases
+    val fetchCharactersUseCase = FetchCharactersUseCase(repository)
+    val fetchCharacterDetailsUseCase = FetchCharacterDetailsUseCase(repository)
+
+    // Initialize the ViewModels
+    val characterViewModel = CharacterViewModel(fetchCharactersUseCase)
+    val marvelsScreen = Marvels_Screen(fetchCharacterDetailsUseCase)
+
+    // Return the ViewModels as a pair
+    return Pair(characterViewModel, marvelsScreen)
+}
