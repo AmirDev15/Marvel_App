b392f7ac566f394909355ac198b25e0c287da7e1
Amir <amirfadhil.abd@gmail.com>
2024-08-29 07:44:21 +0300

    updating repository class for managing request data from api and database

---
+++ app/src/main/java/com/example/marvel_app/data/repository/MarvelRepositoryImpl.kt
@@ -0,0 +1,150 @@
+package com.example.marvel_app.data.repository
+
+import com.example.marvel_app.data.data_source.local.database.dao.CharacterDao
+import com.example.marvel_app.data.data_source.local.database.dao.ComicDao
+import com.example.marvel_app.data.data_source.local.database.dao.EventDao
+import com.example.marvel_app.data.data_source.local.database.dao.SeriesDao
+import com.example.marvel_app.data.data_source.local.database.mapper.characterEntityToDomain
+import com.example.marvel_app.data.data_source.local.database.mapper.comicEntityToDomain
+import com.example.marvel_app.data.data_source.local.database.mapper.eventEntityToDomain
+import com.example.marvel_app.data.data_source.local.database.mapper.mapToEntityComics
+import com.example.marvel_app.data.data_source.local.database.mapper.mapToEntityEvents
+import com.example.marvel_app.data.data_source.local.database.mapper.mapToEntitySeries
+import com.example.marvel_app.data.data_source.local.database.mapper.responseCharacterToDomain
+import com.example.marvel_app.data.data_source.local.database.mapper.responseCharacterToEntity
+import com.example.marvel_app.data.data_source.local.database.mapper.seriesEntityToDomain
+import com.example.marvel_app.data.data_source.remote.Api_service.Marvel_api_service
+//import com.example.marvel_app.data.mapper.mapToDomainCharacter
+import com.example.marvel_app.data.mapper.mapToDomainCharacterDetails
+import com.example.marvel_app.domain.model.CharacterData
+import com.example.marvel_app.domain.model.Marvels_Data
+import com.example.marvel_app.domain.repository.MarvelRepository_domain
+import com.example.marvel_app.util.generateHash
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+
+
+class MarvelRepositoryImpl(
+    private val apiService: Marvel_api_service,
+    private val characterDao: CharacterDao,
+    private val comicDao: ComicDao,
+    private val seriesDao: SeriesDao,
+    private val eventDao: EventDao,
+) : MarvelRepository_domain {
+
+    private val ts = System.currentTimeMillis().toString()
+    private val privateKey = "9c3de5724690bf008ef09484f4e6cf283b6707b9"
+    private val publicKey = "0de055665d65a7da0a64fc7e494ed135"
+    private val hash = generateHash(ts, privateKey, publicKey)
+
+    override suspend fun fetchCharacters(
+        limit: Int,
+        offset: Int,
+        term: String?,
+    ): List<CharacterData> {
+        val characters = characterDao.searchCharactersByName(term)
+//        characterDao.deleteAll()
+        if (characters.isNotEmpty()) {
+
+            return characterEntityToDomain(characters)
+        } else {
+
+            val fetchedCharacters = withContext(Dispatchers.IO) {
+                val response = apiService.getCharacters(limit, offset, term, ts, publicKey, hash)
+
+                if (response.body()?.data?.results.isNullOrEmpty()) {
+
+                    emptyList()
+                } else {
+                    response.body()?.let {
+                        val characterEntities = responseCharacterToEntity(it)
+                        characterDao.insertCharacters(characterEntities)
+                        responseCharacterToDomain(it)
+
+                    } ?: emptyList()
+                }
+            }
+            return fetchedCharacters
+        }
+    }
+
+    override suspend fun fetchComicsAndSeries(characterId: Int): Triple<List<Marvels_Data>, List<Marvels_Data>, List<Marvels_Data>> {
+
+
+        try {
+
+//            comicDao.deleteAll()
+//            seriesDao.deleteAll()
+//            eventDao.deleteAll()
+
+            val comics = comicDao.getComicsForCharacter(characterId)
+            val series = seriesDao.getSeriesForCharacter(characterId)
+            val events = eventDao.getEventsForCharacter(characterId)
+
+            if (comics.isNotEmpty() && series.isNotEmpty() && events.isNotEmpty()) {
+
+                return Triple(
+                    comicEntityToDomain(comics),
+                    seriesEntityToDomain(series),
+                    eventEntityToDomain(events)
+                )
+            } else {
+
+                val startTime = System.currentTimeMillis()
+
+                val (apiComics, apiSeries, apiEvents) = withContext(Dispatchers.IO) {
+
+                    // Start timing individual requests
+                    val comicsStartTime = System.currentTimeMillis()
+                    val seriesStartTime = System.currentTimeMillis()
+                    val eventsStartTime = System.currentTimeMillis()
+
+                    val comicsDeferred = async { apiService.getComicsForCharacter(characterId) }
+                    val seriesDeferred = async { apiService.getSeriesForCharacter(characterId) }
+                    val eventsDeferred = async { apiService.getEventsForCharacter(characterId) }
+
+                    val comicsResponse = comicsDeferred.await()
+                    val seriesResponse = seriesDeferred.await()
+                    val eventsResponse = eventsDeferred.await()
+
+                    if (comicsResponse.isSuccessful) {
+
+                        val comicsData = comicsResponse.body()?.let {
+                            val comicEntities = mapToEntityComics(it, characterId)
+                            comicDao.insertComics(comicEntities)
+                            mapToDomainCharacterDetails(it, characterId)
+                        } ?: emptyList()
+                        val seriesData = seriesResponse.body()?.let {
+                            val seriesEntities = mapToEntitySeries(it, characterId)
+                            seriesDao.insertSeries(seriesEntities)
+                            mapToDomainCharacterDetails(it, characterId)
+                        } ?: emptyList()
+                        val eventsData = eventsResponse.body()?.let {
+
+                            val eventEntities = mapToEntityEvents(it, characterId)
+                            eventDao.insertEvents(eventEntities)
+                            mapToDomainCharacterDetails(it, characterId)
+                        } ?: emptyList()
+
+                        Triple(comicsData, seriesData, eventsData)
+                    } else {
+                        Triple(emptyList(), emptyList(), emptyList())
+                    }
+
+                }
+                // Calculate total duration and average time
+                val endTime = System.currentTimeMillis()
+                val totalDurationMillis = endTime - startTime
+                val totalDurationMinutes = totalDurationMillis / 60000.0
+                return Triple(apiComics, apiSeries, apiEvents)
+
+            }
+        } catch (e: Exception) {
+        }
+        return Triple(emptyList(), emptyList(), emptyList())
+
+    }
+
+
+}
