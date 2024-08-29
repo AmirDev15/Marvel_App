f05627911e46191c38a189fdeca3da27889ccf1e
Amir <amirfadhil.abd@gmail.com>
2024-08-29 07:46:34 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/marvel_app/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/marvel_app/presentation/ui/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.example.marvel_app.presentation.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.marvel_app.presentation.ui.HomePage.MarvelHomePage
+import com.example.marvel_app.presentation.navigation.Navigation
+import com.example.marvel_app.util.initializeDependencies
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MarvelHomePage()
-
+        try {
+            // Initialize dependencies and get the ViewModels
+            val (characterViewModel, characterDetailsViewModel) = initializeDependencies(this)
+            // Set the content with the initialized ViewModels
+            setContent {
+                Navigation(
+                    viewModel = characterViewModel,
+                    Marvels_Screen = characterDetailsViewModel
+                )
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.e("MainActivity", "Error initializing dependencies: ${e.message}")
         }
     }
 }
