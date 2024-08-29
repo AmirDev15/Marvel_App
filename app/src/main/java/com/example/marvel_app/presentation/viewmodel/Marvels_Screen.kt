b6f33d5a54b5d34f6c4f7cac095149a262972b11
Amir <amirfadhil.abd@gmail.com>
2024-08-29 07:47:07 +0300

    Create Marvels_Screen.kt

---
+++ app/src/main/java/com/example/marvel_app/presentation/viewmodel/Marvels_Screen.kt
@@ -0,0 +1,49 @@
+package com.example.marvel_app.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.marvel_app.domain.model.Marvels_Data
+import com.example.marvel_app.domain.usecase.FetchCharacterDetailsUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class Marvels_Screen(
+    val fetchComicsAndSeriesUseCase: FetchCharacterDetailsUseCase,
+) : ViewModel() {
+
+    private val _comics = MutableStateFlow<List<Marvels_Data>>(emptyList())
+    val comics: StateFlow<List<Marvels_Data>> get() = _comics
+
+    private val _series = MutableStateFlow<List<Marvels_Data>>(emptyList())
+    val series: StateFlow<List<Marvels_Data>> get() = _series
+
+    private val _events = MutableStateFlow<List<Marvels_Data>>(emptyList())
+    val events: StateFlow<List<Marvels_Data>> get() = _events
+
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> get() = _isLoading
+
+
+    fun fetchComicsAndSeries(characterId: Int) {
+        viewModelScope.launch {
+            _isLoading.value = true
+
+            _comics.value = emptyList()
+            _series.value = emptyList()
+            _events.value = emptyList()
+            try {
+                val (comics, series, events) = fetchComicsAndSeriesUseCase.execute(characterId)
+                _comics.value = comics
+                _series.value = (series)
+                _events.value = (events)
+            } catch (e: Exception) {
+            } finally {
+                _isLoading.value = false
+            }
+
+        }
+    }
+}
+
+
