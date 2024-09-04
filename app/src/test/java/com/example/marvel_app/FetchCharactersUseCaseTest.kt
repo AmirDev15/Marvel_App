58aad6b28a266f4fc5dabe2199b44c95b27db421
Amir <amirfadhil.abd@gmail.com>
2024-09-05 00:03:42 +0300

    creating FetchCharactersUseCaseTest

---
+++ app/src/test/java/com/example/marvel_app/FetchCharactersUseCaseTest.kt
@@ -0,0 +1,20 @@
+package com.example.marvel_app
+
+import com.example.marvel_app.domain.model.CharacterData
+import com.example.marvel_app.domain.repository.MarvelRepository_domain
+import com.example.marvel_app.domain.usecase.FetchCharactersUseCase
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+
+// Use case test
+class FetchCharactersUseCaseTest {
+
+
+
+
+}
