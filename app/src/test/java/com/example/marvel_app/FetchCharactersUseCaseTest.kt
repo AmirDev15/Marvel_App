c27c1ba84f28d85a9499a916b04acf9e8d3d324e
Amir <amirfadhil.abd@gmail.com>
2024-09-05 00:05:00 +0300

    creating unit test fun for FetchCharactersUseCase fun Invoke

--- app/src/test/java/com/example/marvel_app/FetchCharactersUseCaseTest.kt
+++ app/src/test/java/com/example/marvel_app/FetchCharactersUseCaseTest.kt
@@ -14,7 +14,33 @@ import org.mockito.Mockito.`when`
 // Use case test
 class FetchCharactersUseCaseTest {
 
+    private lateinit var mockRepository: MarvelRepository_domain
+    private lateinit var useCase: FetchCharactersUseCase
+
+    @Before
+    fun setUp() {
+        mockRepository = mock()
+        useCase = FetchCharactersUseCase(mockRepository)
+    }
+
+    @Test
+    fun `invoke returns characters from repository`() = runBlocking {
 
 
+        val characterName = "3-d man"
+        val characterData = CharacterData(
+            id = 1,
+            name = characterName,
+            description = "Genius billionaire",
+            imageUrl = "image_url"
+        ) // Populate with actual test data
+        `when`(mockRepository.fetchCharacters(10, 0, characterName)).thenReturn(listOf(characterData))
+
+        // When
+        val result = useCase(limit = 10, offset = 0, term = characterName)
+
+        // Then
+        assertEquals(listOf(characterData) , result)
+    }
 
 }
