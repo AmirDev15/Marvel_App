0db8c367bc21c580d044693f017983dc2401394c
Amir <amirfadhil.abd@gmail.com>
2024-09-07 02:50:55 +0300

    re allocating all data classes on one kotlin file

--- app/src/main/java/com/example/marvel_app/data/data_source/remote/Api_response_Dto/CharacterDataDTO.kt
+++
@@ -1,9 +0,0 @@
-package com.example.marvel_app.data.data_source.remote.Api_response_Dto
-
-data class CharacterDataDTO(
-//    val offset: Int,
-//    val limit: Int,
-//    val total: Int,
-//    val count: Int,
-    val results: List<MarvelCharacterDTO>
-)
