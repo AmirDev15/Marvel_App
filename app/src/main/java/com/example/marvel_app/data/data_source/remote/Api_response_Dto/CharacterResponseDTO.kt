fc912ea3e2d6dbb8e09adae595eecbba17e1d90e
Amir <amirfadhil.abd@gmail.com>
2024-09-07 03:03:48 +0300

    removing data classes and moving them to one kotlin file

--- app/src/main/java/com/example/marvel_app/data/data_source/remote/Api_response_Dto/CharacterResponseDTO.kt
+++
@@ -1,5 +0,0 @@
-package com.example.marvel_app.data.data_source.remote.Api_response_Dto
-
-data class CharacterResponseDTO(
-    val data: CharacterDataDTO
-)
--- app/src/main/java/com/example/marvel_app/data/data_source/remote/Api_response_Dto/ThumbnailDTO.kt
+++
@@ -1,6 +0,0 @@
-package com.example.marvel_app.data.data_source.remote.Api_response_Dto
-
-data class ThumbnailDTO(
-    val path: String,
-    val extension: String
-)
