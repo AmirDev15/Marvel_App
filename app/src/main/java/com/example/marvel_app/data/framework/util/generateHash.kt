0232d49356271f6a9ae65566a629d3fcedafcd2b
Amir <amirfadhil.abd@gmail.com>
2024-09-09 23:27:52 +0300

    refactor and moving to specify loc

---
+++ app/src/main/java/com/example/marvel_app/data/framework/util/generateHash.kt
@@ -0,0 +1,12 @@
+package com.example.marvel_app.data.framework.util
+
+
+
+import java.security.MessageDigest
+
+fun generateHash(ts: String, privateKey: String, publicKey: String): String {
+    val input = "$ts$privateKey$publicKey"
+    val md = MessageDigest.getInstance("MD5")
+    val digest = md.digest(input.toByteArray())
+    return digest.joinToString("") { String.format("%02x", it) }
+}
