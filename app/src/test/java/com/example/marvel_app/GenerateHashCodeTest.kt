a13cb40cac8080d1138215fc3843e17467d45f18
Amir <amirfadhil.abd@gmail.com>
2024-09-11 15:43:11 +0300

    Update GenerateHashCodeTest.kt

--- app/src/test/java/com/example/marvel_app/GenerateHashCodeTest.kt
+++ app/src/test/java/com/example/marvel_app/GenerateHashCodeTest.kt
@@ -11,8 +11,9 @@ class GenerateHashCodeTest {
     fun `generateHash returns correct MD5 hash`() {
 
         val ts = "123432"
-        val privateKey = "9c3de5724690bf008ef09484f4e6cf283b6707b9"
-        val publicKey = "0de055665d65a7da0a64fc7e494ed135"
+        val publicKey = BuildConfig.PUBLIC_API_KEY
+        val privateKey = BuildConfig.PRIVATE_KEY
+
 
         val expectedHash = "fa3a11a00b317a11e8f94cc727283381"
 
